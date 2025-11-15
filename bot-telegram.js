f2a61c3da2f16bd26006cb5d6aa51b4487cdeebc
sebas2104495 <62824350+sebas2104495@users.noreply.github.com>
2025-11-15 07:06:47 -0500

    Update bot-telegram.js

--- bot-telegram.js
+++ bot-telegram.js
@@ -52,13 +52,17 @@ async function obtenerTareasDeMoodle() {
 
     const browser = await puppeteer.launch({
         headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
             '--disable-gpu',
             '--disable-software-rasterizer',
-            '--disable-extensions'
+            '--disable-extensions',
+            '--disable-web-security',
+            '--disable-features=IsolateOrigins,site-per-process',
+            '--single-process'
         ]
     });
 
